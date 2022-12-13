a0a3407a1a6e8ab451bb6ce12bfec1e92db1d0f7
Tomas Fisher <dontomassino777@gmail.com>
2022-12-13 09:38:57 -0700

    html structured

---
+++ index.js
@@ -0,0 +1,5 @@
+const textForm = document.getElementById("text-form")
+const inputBox = document.getElementById("input-box")
+const readButton = document.getElementById("read-button")
+const readerDiv = document.getElementById("reader-div")
+
