f5be32d480f1740c170752a99be9a56df6a53513
Tomas Fisher <dontomassino777@gmail.com>
2022-12-13 10:00:25 -0700

    submitHandler added

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const textForm = document.getElementById("text-form")
-const inputBox = document.getElementById("input-box")
+const inputText = document.getElementById("input-text")
 const readButton = document.getElementById("read-button")
 const readerDiv = document.getElementById("reader-div")
 
+function processText() {
+
+}
+
+function submitHandler(event) {
+    event.preventDefault()
+
+    // let textToBeRead = document.getElementById("input-text")
+
+    let textObject = {
+        inputText: inputText.value
+    }
+
+    processText(textObject)
+    
+    inputText.value = ''
+}
+
+textForm.addEventListener('submit', submitHandler)
